5f98aa3938ca6c607bc109cc27a692f893b4366a
Adeepa Gunathilake <iceblob@proton.me>
2024-04-17 21:08:49 +0530

    Fixed some mobile responsive issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,24 +40,25 @@ export default function Home() {
                 <div className="thin-line mb-[30px]"></div>
 
                 <div 
-                    className="flex gap-5 gap-y-12 justify-center flex-wrap
+                    className="flex gap-5 gap-y-7 justify-center flex-wrap
                         max-w-[550px] mx-auto min-[660px]:max-w-[590px] min-[1248px]:max-w-fit">
                     { package_cards }
                     { package_cards.length % 2 === 0 ? null : dull_package_card }
                 </div>
             </div>
-            <div className="mt-[70px]">
+            <div className="mt-[30px] min-[680px]:mt-[70px]">
                 <h2 className="text-3xl mb-[5px] text-center text-neutral-200">
                     Buy what you want! 
                 </h2>
                 <div className="thin-line mb-[30px]"></div>
 
                 <div 
-                    className="flex gap-5 gap-y-12 justify-center flex-wrap
+                    className="flex gap-5 gap-y-5 justify-center flex-wrap
                         max-w-[550px] mx-auto min-[660px]:max-w-[590px] min-[1248px]:max-w-fit">
                     { item_cards }
                 </div>
             </div>
+            <div className="h-[30px]"></div>
         </section>
         </>
     );
