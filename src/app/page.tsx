63280b37ce6982e42eedc6637fd57d3f48764c47
Adeepa Gunathilake <iceblob@proton.me>
2024-04-17 15:05:05 +0530

    Changed indentation to 4 spaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,21 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata : Metadata = {
-  title: "Codesprint Store",
+    title: "Codesprint Store",
 }
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center flex-1 text-center">
-        <h1 className="text-6xl font-bold">
-          <span className="text-green-600">Launching</span> Soon !!!
-        </h1>
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <main className="flex flex-col items-center justify-center flex-1 text-center">
+                <h1 className="text-6xl font-bold">
+                    <span className="text-green-600">Launching</span> Soon !!!
+                </h1>
 
-        <p className="mt-3 text-2xl">
-          We are working hard to bring you our new site. Stay tuned!
-        </p>
-      </main>
-    </div>
-  );
+                <p className="mt-3 text-2xl">
+                    We are working hard to bring you our new site. Stay tuned!
+                </p>
+            </main>
+        </div>
+    );
 }
