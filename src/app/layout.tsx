c550d8a2f4c83f0e4d2c191d641cdb542aafeaab
Kavishcan <kavishcan2002@gmail.com>
2024-04-18 20:43:38 +0530

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,25 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import SiteHeader from "@/components/SiteHeader";
+import SiteFooter from "@/components/SiteFooter";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Codesprint Merch Store",
-    description: "Official Merchandise store fore Codesprint 8.0",
+  title: "Codesprint Merch Store",
+  description: "Official Merchandise store fore Codesprint 8.0",
 };
 
-type RootLayoutProps = Readonly<{ children: React.ReactNode; }>;
+type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <SiteHeader />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SiteHeader />
+        {children}
+        <SiteFooter />
+      </body>
+    </html>
+  );
 }
