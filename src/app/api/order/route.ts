e61f8fb67a8144b69341ed928988d353f7bf6dd9
Adeepa Gunathilake <iceblob@proton.me>
2024-05-01 13:40:40 +0530

    changed order encoding mechanism

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -24,44 +24,6 @@ function getInvoiceID() {
     return `${raw_id.slice(0, 4)}-${raw_id.slice(4, 8)}`
 }
 
-function getOrderStr(orders: any[]) {
-    const orderArray : Record<string, number> = {};
-
-    const size_offsets = { "S": 0, "M": 1, "L": 2, "XL": 3, "2XL": 4 };
-    const item_indices = { "Hoodie": 0, "T-Shirt": 10, "Notebook": 25, "Wristband": 26 };
-    const hoodie_colors = { "Black": 0, "White": 5 };
-    const tshirt_colors = { "Black": 0, "Ash": 5, "Blue": 10 };
-
-    orders.forEach(order => {
-        let index = 0, offset_1 = 0, offset_2 = 0;
-
-        const item = order.item;
-        const quantity = order.quantity;
-
-        index = item_indices[item as keyof typeof item_indices];
-
-        if (item === "Hoodie" || item === "T-Shirt") {
-            offset_2 = size_offsets[order.size as keyof typeof size_offsets];
-        }
-
-        if (item === "Hoodie") {
-            offset_1 = hoodie_colors[order.color as keyof typeof hoodie_colors];
-        } else if (item === "T-Shirt") {
-            offset_1 = tshirt_colors[order.color as keyof typeof tshirt_colors];
-        }
-
-        orderArray[`${index + offset_1 + offset_2}`] = quantity;
-    });
-
-    let order_str = "";
-    for (let i = 0; i < 27; i++) {
-        order_str += orderArray[`${i}`] ? orderArray[`${i}`] : 0;
-        order_str += i === 26 ? "" : ",";
-    }
-
-    return order_str;
-}
-
 function getParams(requestBody: any) : GetParamsResult {
     const orders = requestBody['order'];
     const name = requestBody['name']! as string;
