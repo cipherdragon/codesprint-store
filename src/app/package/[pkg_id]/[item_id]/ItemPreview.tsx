e08accc51b0e3d323bde1e4deaa069a9795c1804
Adeepa Gunathilake <iceblob@proton.me>
2024-05-03 23:54:16 +0530

    Add swipe gestures to ItemPreview component

--- src/app/package/[pkg_id]/[item_id]/ItemPreview.tsx
+++ src/app/package/[pkg_id]/[item_id]/ItemPreview.tsx
@@ -9,6 +9,8 @@ interface ItemPreviewProps {
 
 export default function ItemPreview({ item }: ItemPreviewProps) {
     const [thumbIndex, setThumbIndex] = useState(0);
+    const [isDragging, setIsDragging] = useState(false);
+    const [swipeStart, setSwipeStart] = useState(0);
 
     const thumbnails = item.thumbnail.map((src, index) => 
         <img key={index} src={src} alt={item.name} width={60} height={60}
@@ -18,6 +20,40 @@ export default function ItemPreview({ item }: ItemPreviewProps) {
                 max-sm:h-[3px] max-sm:bg-greeen-800 overflow-hidden max-sm:rounded-none`}
             onClick={_ => setThumbIndex(index)} />
     );
+
+    type SwipGuestureParam = React.TouchEvent<HTMLImageElement> | 
+        React.MouseEvent<HTMLImageElement>
+
+    const swipeGuesture = (e : SwipGuestureParam) => {
+        e.preventDefault();
+
+        let clientX = ("touches" in e && e.touches.length) ? 
+            e.touches[0].clientX : ((e as any).clientX ?? 0);
+
+        const controlEvents = ["mousedown", "mouseup", "mouseleave", "touchstart", "touchend"];
+        if (controlEvents.includes(e.type)) {
+            setIsDragging(e.type === "mousedown" || e.type === "touchstart");
+            setSwipeStart(clientX - e.currentTarget.x);
+            return;
+        }
+        
+        if (!isDragging) return;
+
+        const x = clientX - e.currentTarget.x;
+        const swipe = (x - swipeStart) * 100 / e.currentTarget.width;
+
+        const direction = swipe < 0 ? 1 : -1; // Left -1, Right 1
+
+        if (direction === -1 && thumbIndex < 1) return;
+        if (direction === 1 && thumbIndex >= item.thumbnail.length - 1) return;
+
+        const shouldSwipe = Math.abs(swipe) > 20;
+
+        if (!shouldSwipe) return;
+        
+        setThumbIndex(thumbIndex + direction);
+        setIsDragging(false);
+    }
 
     return (
         <>
@@ -36,8 +72,17 @@ export default function ItemPreview({ item }: ItemPreviewProps) {
                 alt={item.name} 
                 width={400} 
                 height={400}
-                className="max-w-[400px] max-h-[400px] object-cover rounded-3xl
-                    max-[620px]:w-[300px] max-[620px]:h-[300px]" />
+                className={`max-w-[400px] max-h-[400px] object-cover rounded-3xl
+                    max-[620px]:w-[300px] max-[620px]:h-[300px]
+                    ${thumbnails.length > 1 && "cursor-grab" || ""}`}
+                onDragStart={e => e.preventDefault()}
+                onMouseDown={swipeGuesture}
+                onMouseMove={swipeGuesture}
+                onMouseUp={swipeGuesture}
+                onMouseLeave={swipeGuesture}
+                onTouchStart={swipeGuesture}
+                onTouchEnd={swipeGuesture}
+                onTouchMove={swipeGuesture} />
             <h2 
                 className="bg-[#36c0a9] bg-opacity-50 border border-[#36c0a9] 
                 backdrop-blur-md shadow-md shadow-[#000000a6]
