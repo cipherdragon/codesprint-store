02820a34860377e767dcf63295e7c7202077d8ba
Adeepa Gunathilake <iceblob@proton.me>
2024-06-03 22:19:48 +0530

    final commit
    store officially closes. most probably, this is the final commit!

--- src/app/package/[pkg_id]/checkout/thankyou/page.tsx
+++ src/app/package/[pkg_id]/checkout/thankyou/page.tsx
@@ -10,32 +10,33 @@ export default function ThankYou() {
                 <h1 className='sm:text-3xl text-2xl text-center font-bold mb-[20px]'>
                     Thank you for your purchase!
                 </h1>
-                <p className='text-lg max-w-[600px] mb-[20px] sm:text-justify'>
+                <p className='text-lg max-w-[600px] mb-[20px] sm:text-justify line-through text-neutral-500'>
                     We recorded your purchase. By now, your invoice should have been
                     downloaded automatically. Check the invoice for payment bank 
                     account and further details.
                 </p>
-                <p className="text-lg max-w-[600px] sm:text-justify mb-[20px]">
+                <p className="text-lg max-w-[600px] sm:text-justify mb-[20px] line-through text-neutral-500">
                     After you complete your payment, please submit the deposit slip and 
-                    the invoice to&nbsp;
-                    <a 
-                        className="text-teal-300 cursor-pointer"
-                        href="https://codesprint.lk/store-checkout">
-                        https://codesprint.lk/store-checkout
-                    </a>
+                    the invoice to https://codesprint.lk/store-checkout
+                </p>
+                <p className='text-lg max-w-[600px] mb-[20px] sm:text-justify'>
+                    Codesprint merch store is officially closed now. We are no longer
+                    accepting orders or payments. Site's functionality is preserved
+                    only for archival purposes. 
+                </p>
+                <p className='text-lg max-w-[600px] mb-[20px] sm:text-justify'>
+                    Thank you for your support!
                 </p>
 
                 <div className="max-w-[600px] w-full flex gap-[10px] 
                     flex-wrap justify-center sm:justify-between">
-                    <a href="https://codesprint.lk/store-checkout" className="cursor-pointer">
-                        <button 
-                            className="font-bold text-white bg-teal-600 p-3 flex gap-[10px]
-                                rounded-lg w-[290px] justify-center items-center mb-[10px]
-                                border-teal-600 border-2">
-                            Submit Payment Receipt!
-                            <CheckCircle />
-                        </button>
-                    </a>
+                    <button 
+                        className="font-bold text-white bg-teal-600 p-3 flex gap-[10px]
+                            rounded-lg w-[290px] justify-center items-center mb-[10px]
+                            border-teal-600 border-2">
+                        Submit Payment Receipt!
+                        <CheckCircle />
+                    </button>
                     <a className="cursor-pointer" href="/">
                         <button 
                             className="font-bold text-teal-400 p-3 flex gap-[10px]
--- src/util/invoice/PackageInvoice.ts
+++ src/util/invoice/PackageInvoice.ts
@@ -4,18 +4,9 @@ import { ShoppingCartType } from "@/db/ShoppingCart";
 import PackageData from "@/types/PackageData";
 
 const order_instructions = 
-    "Keep this invoice for later reference. \n\n" + 
-    "To complete the order, please deposit the payment to the bank account given " + 
-    "below. Then submit this invoice and the bank receipt/screen shot/proof of payment " + 
-    "to https://codesprint.lk/store-checkout. \n\n" + 
-
-    "Acc no: 8001874231\n" +
-    "Name: V Kavishcan\n" +
-    "Bank: Commercial Bank\n" +
-    "Branch: Wellawatta\n\n" +
-    
-    "Note: All payments must be completed by midnight, 3rd June 2024.\n" +
-    "Any payments made after will be refunded and orders will not be considered.\n\n"
+    "YOUR ORDER WILL NOT BE DELIVERED.\n\n" +
+    "We are no longer accepting orders. Site's functionality is\n" +
+    "maintained only for archival purposes. Thank you!\n"
 
 export class PackageInvoice {
     private item_list : ShoppingCartType;
